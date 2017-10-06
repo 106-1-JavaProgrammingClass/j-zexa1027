bb22c0b5bac7ad287b86a2e39e8820f4bd8f609b
zexa1027 <32056794+zexa1027@users.noreply.github.com>
2017-10-06 11:15:24 +0800

    Add files via upload

---
+++ src/HW1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public  class HW1 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int mile = scn.nextInt();
+        System.out.println(mile * 1.6);
+    }
+}
